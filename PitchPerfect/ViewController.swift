fa3bcd439ce57e66f57854b8266b8c8c987dc926
javier <javier@dojodevs.io>
2021-09-23 21:39:36 -0500

    just added basic interaction and constrains to buttons and labels

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ecordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed")
+        ecordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Record button was pressed")
+        ecordingLabel.text = "Tap to Record"
+    }
+    
 }
 

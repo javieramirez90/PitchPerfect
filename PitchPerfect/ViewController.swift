7a1aed6a49d90ba1814abc53f85c4a7815d6ef31
javier <javier@dojodevs.io>
2021-09-23 23:38:37 -0500

    Added assets and fixed constrains

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -8,13 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var ecordingLabel: UILabel!
     
+    @IBOutlet weak var ecordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
     @IBAction func recordAudio(_ sender: Any) {
         print("Record button was pressed")
         ecordingLabel.text = "Recording in Progress"
